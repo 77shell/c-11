5069ff1f79cd671b65982f17748f5291e26af5f9
77shell <77shell@gmail.com>
2018-10-07 22:51:23 +0800

    Practice exception

--- exception_13_5_2.cpp
+++ exception_13_5_2.cpp
@@ -4,43 +4,65 @@
 
 using namespace std;
 
+class A {
+public:
+	A(int a)
+		: name(a)
+		{}
+	int name;		
+};
+
+struct S {
+	int s;
+};
+
 class E {
 public:
-   E() {}
-   virtual void do_something(void) { std::cout << "E: Do something" << std:: endl; }
+	E()
+		try
+			: a(0), s{10}
+			  {
+			  }
+	catch(std::exception) {
+		cout << "std::exception" << endl;
+	}
+	
+	A a;
+	S s;
+	virtual void do_something(void) { std::cout << "E: Do something" << a.name << s.s << std:: endl; }
 };
 
 
 class F : public E {
 public:
-   F() {}
-   virtual void do_something(void) { std::cout << "F: Do something" << std::endl; }
+	F() {}
+	virtual void do_something(void) { std::cout << "F: Do something" << std::endl; }
 };
    
 
 void f()
 {
-   std::cout << "Hello" << std::endl;
-   throw F{};
+	std::cout << "Hello" << std::endl;
+	throw F{};
 }
 
 
 int main(int argc, char *argv[])
-   try {
-      f();
-      exit(EXIT_SUCCESS);
-   }
-   catch(E obj) {
-      obj.do_something();
-   }
-   catch(E) {
-      cout << "Catch you" << endl;
-      exit(EXIT_SUCCESS);
-   }
-   catch(F obj) {
-      obj.do_something();
-   }
-   catch(...) {
-      cout << "Catch everything" << endl;
-   }
+	try {
+		f();
+		exit(EXIT_SUCCESS);
+	}
+	catch(E obj) {
+		obj.do_something();
+	}
+	catch(E) {
+		cout << "Catch you" << endl;
+		exit(EXIT_SUCCESS);
+	}
+	catch(F obj) {
+		obj.do_something();
+	}
+	catch(...) {
+		cout << "Catch everything" << endl;
+	}
 
