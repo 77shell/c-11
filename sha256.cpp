f3333ea999f698da22f8a1f8537b0402bd59dd93
Max (HP-Z240) <max.yang@deltaww.com>
2021-10-21 10:25:45 +0800

    Improve SHA256 implementation

--- sha256.cpp
+++ sha256.cpp
@@ -5,13 +5,47 @@
 #include <openssl/evp.h>
 #include <cstdio>
 #include <cstring>
+#include <string>
+#include <memory>
 
 static void digest_message(const unsigned char *message, size_t message_len, unsigned char **digest, unsigned int *digest_len);
 static void digest_message_file(FILE *f, unsigned char **digest, unsigned int *digest_len);
 
-struct SHA256 {
-	SHA256(const char *file)
-		: f {nullptr}, f_len {0}, digest {nullptr}, digest_len {0}, mdctx {nullptr}, message_buf {nullptr}
+namespace util {
+
+	struct SHA256 {
+		SHA256(const char *file);
+		~SHA256();
+
+		std::string digest() const;
+		unsigned int digest_len() const;
+		uint8_t operator[](size_t idx);
+
+		/*
+		 * 0: successful
+		 * 0<: failed
+		 */
+		int write_to_file(const char *file_path);
+
+		/*
+		 * 0: equal
+		 * >0: not equal
+		 */
+		int compare(const char *sha_file);
+		int compare(const void *sha);
+
+	private:
+		struct impl_t;
+		std::unique_ptr<impl_t> mp_Impl;
+	};
+
+}
+
+using namespace util;
+
+struct SHA256::impl_t {
+	impl_t(const char *file)
+		: f {nullptr}, f_len {0}, digest {nullptr}, digest_len {0}, mdctx {nullptr}, message_buf {new unsigned char [BUFSIZ]}
 		{
 			f = fopen(file, "r");
 			if (!f) {
@@ -31,21 +65,18 @@ struct SHA256 {
 			if((mdctx = EVP_MD_CTX_new()) == NULL)
 				return;
 
-			message_buf = malloc(BUFSIZ);
-			if(!message_buf) {
-				perror("Locate message buffer");
-				return;
-			}
-
 			digest_message();
+			format_digest();
 		}
 
-	~SHA256() {
+	~impl_t() {
+		message_buf.reset();
+
 		if(f)
 			fclose(f);
 
 		if(digest)
-		 	OPENSSL_free(digest);
+			OPENSSL_free(digest);
 
 		if(mdctx)
 			EVP_MD_CTX_free(mdctx);
@@ -58,11 +89,11 @@ struct SHA256 {
 		long len {f_len};
 		do {
 			unsigned int message_len = len > BUFSIZ ? BUFSIZ : len;
-			size_t rlen = fread(message_buf, sizeof(unsigned char), message_len, f);
+			size_t rlen = fread(message_buf.get(), sizeof(unsigned char), message_len, f);
 			if(rlen != message_len)
 				return -2;
 
-			if(EVP_DigestUpdate(mdctx, message_buf, message_len) != 1)
+			if(EVP_DigestUpdate(mdctx, message_buf.get(), message_len) != 1)
 				return -3;
 
 			if(len > BUFSIZ)
@@ -76,17 +107,207 @@ struct SHA256 {
 
 		if(EVP_DigestFinal_ex(mdctx, digest, &digest_len) != 1)
 			return -5;
-
+		
 		return 0;
+	}
+
+	void format_digest() {
+		size_t buf_size {digest_len * 2 + 1}; // Each byte consists of two characters, one is for null-terminator
+		std::unique_ptr<char[]> buf {new char [buf_size]};
+		char *ptr {buf.get()};
+
+		std::memset(buf.get(), 0, buf_size);
+		for(unsigned int len=0; len<digest_len; len++)
+			ptr += sprintf(ptr, "%02x", digest[len]);
+		digest_str = buf.get();
 	}
 
 	FILE *f;
 	long f_len;
 	unsigned char *digest;
 	unsigned int digest_len;
+	std::string digest_str;
 	EVP_MD_CTX *mdctx;
-	void *message_buf;
+	std::unique_ptr<unsigned char[]> message_buf;
 };
+
+SHA256::SHA256(const char *file)
+	: mp_Impl {new impl_t {file}}
+{}
+
+SHA256::~SHA256() {
+	mp_Impl.reset();
+}
+
+std::string
+SHA256::digest() const
+{
+	return mp_Impl->digest_str;
+}
+
+unsigned int
+SHA256::digest_len() const
+{
+	return mp_Impl->digest_len;
+}
+
+uint8_t
+SHA256::operator[](size_t data_idx)
+{
+	return data_idx < mp_Impl->digest_len
+		? mp_Impl->digest[data_idx]
+		: 0xff;
+}
+	
+int
+SHA256::write_to_file(const char *file_path)
+{
+	FILE *f = fopen(file_path, "w+");
+	if(!f) {
+		fprintf(stderr, "%s: couldn't open %s\n", __func__, file_path);
+		perror(__func__);
+		return -1;
+	}
+
+	fwrite(mp_Impl->digest, sizeof(unsigned char), mp_Impl->digest_len, f);
+	fclose(f);
+
+	printf("%s: Digest len %d-byte\n", __func__, mp_Impl->digest_len);
+	for(unsigned int len=0; len<mp_Impl->digest_len; len++)
+		fprintf(stderr, "%02x", mp_Impl->digest[len]);
+	putchar('\n');
+
+	return 0;
+}
+
+int
+SHA256::compare(const char *sha_file)
+{
+	FILE *f = fopen(sha_file, "r");
+	if(!f) {
+		fprintf(stderr, "%s: couldn't open %s\n", __func__, sha_file);
+		perror(__func__);
+		return -1;
+	}
+
+	if(fseek(f, 0L, SEEK_END) != 0)
+		return -2;
+
+	long fsize {ftell(f)};
+	if(fsize == -1L) {
+		perror("Get file size");
+		return -3;
+	}
+	printf("File size: %ld\n", fsize);
+	if(mp_Impl->digest_len != static_cast<unsigned int>(fsize))
+		return 1;
+
+	std::unique_ptr<unsigned char[]> buf {new unsigned char [fsize]};
+	rewind(f);
+	size_t r = fread(buf.get(), fsize, sizeof(unsigned char), f);
+	if(r != static_cast<unsigned int>(fsize)) {
+		fprintf(stderr, "%s: file size is different\n", __func__);
+		return -4;
+	}
+
+	return std::memcmp(mp_Impl->digest, buf.get(), mp_Impl->digest_len);
+}
+	
+int
+SHA256::compare(const void *sha_data)
+{
+	return sha_data
+		? memcmp(sha_data, mp_Impl->digest, mp_Impl->digest_len)
+		: 1;
+}
+
+static int digest_message_file(FILE *f, unsigned char **digest, unsigned int *digest_len, EVP_MD_CTX **);
+
+int
+sha256sum(const char *in_filepath, const char *out_filepath)
+{
+	FILE *f = fopen(in_filepath, "r");
+	if(!f) {
+		fprintf(stderr, "couldn't open %s\n", in_filepath);
+		perror(__func__);
+		return 1;
+	}
+
+	unsigned char *digest {nullptr};
+	unsigned int digest_len;
+	EVP_MD_CTX *mdctx {nullptr};
+	int r = digest_message_file(f, &digest, &digest_len, &mdctx);
+	if(r)
+		printf("%s: calculate %s failed!", __func__, in_filepath);
+
+	if(mdctx)
+		EVP_MD_CTX_free(mdctx);
+
+	fclose(f);
+	if(r) {
+		if(digest)
+			OPENSSL_free(digest);
+		return 2;
+	}
+
+	// Write digest to file
+	f = fopen(out_filepath, "w+");
+	if(!f) {
+		fprintf(stderr, "%s: couldn't open %s\n", __func__, out_filepath);
+		perror(__func__);
+		return 3;
+	}
+	fwrite(digest, sizeof(unsigned char), digest_len, f);
+	fclose(f);
+
+	printf("%s: Digest len %d-byte\n", __func__, digest_len);
+	for(unsigned int len=0; len<digest_len; len++)
+		fprintf(stderr, "%02x", digest[len]);
+	putchar('\n');
+
+	OPENSSL_free(digest);
+	return 0;
+}
+
+static int
+digest_message_file(FILE *f, unsigned char **digest, unsigned int *digest_len, EVP_MD_CTX **pmdctx)
+{
+	EVP_MD_CTX *mdctx = *pmdctx;
+	if((mdctx = EVP_MD_CTX_new()) == NULL)
+		return -1;
+
+	if(EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL) != 1)
+		return -2;
+
+	fseek(f, 0L, SEEK_END);
+	long flen = ftell(f);
+	printf("%s: File size %ld\n", __func__, flen);
+	rewind(f);
+
+	unsigned char message[BUFSIZ];
+	do {
+		unsigned int message_len = flen > BUFSIZ ? BUFSIZ : flen;
+		size_t rlen = fread(message, sizeof(unsigned char), message_len, f);
+		if(rlen != message_len)
+			return -3;
+
+		if(1 != EVP_DigestUpdate(mdctx, message, message_len))
+			return -4;
+
+		if(flen > BUFSIZ)
+			flen -= BUFSIZ;
+		else
+			flen = 0;
+	} while(flen);
+
+	if((*digest = (unsigned char *)OPENSSL_malloc(EVP_MD_size(EVP_sha256()))) == NULL)
+		return -5;
+
+	if(1 != EVP_DigestFinal_ex(mdctx, *digest, digest_len))
+		return -6;
+
+	return 0;
+}
 
 static void
 handleErrors()
@@ -178,30 +399,23 @@ int main(int argc, char **argv)
 	 * OpenSSL 3.0
 	 */
 	printf("OpenSSL 3.0\n");
-	struct SHA256 sha256 {argv[1]};
-	printf("Digest len: %d\n", sha256.digest_len);
-	for(len = 0; len < sha256.digest_len; len++)
-		printf("%02x", sha256.digest[len]);
+	util::SHA256 sha256 {argv[1]};
+	printf("Digest len: %d\n", sha256.digest_len());
+	printf("%s\n", sha256.digest().c_str());
+	unsigned int l {sha256.digest_len()};
+	for(unsigned int i=0; i<l; i++)
+		printf("%02x", sha256[i]);
 	putchar('\n');
 
-	if(memcmp(buffer, sha256.digest, sha256.digest_len) != 0)
+	if(sha256.compare(buffer) != 0)
 		printf("Error, two digests are not equal\n");
 	else
 		printf("OK, two digests are the same\n");
 
-
 	/*----------------------------------------------------
 	 * Write digest to file
 	 */
-	const char *out_filepath = argv[2];
-	f = fopen(out_filepath, "w+");
-	if(!f) {
-		fprintf(stderr, "%s: couldn't open %s\n", __func__, out_filepath);
-		perror(__func__);
-		return 3;
-	}
-	fwrite(sha256.digest, sizeof(unsigned char), sha256.digest_len, f);
-	fclose(f);
+	sha256.write_to_file(argv[2]);
 	return 0;
 }
 
@@ -239,9 +453,3 @@ digest_message(const unsigned char *message, size_t message_len, unsigned char *
 	EVP_MD_CTX_free(mdctx);
 }
 #endif
-
-int
-authenticate_file_sha256(const char *file, const char *digest_file)
-{
-	return 0;
-}
